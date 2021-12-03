643369bca25c55f1e7960b265ad71faf6d0b01d0
aB <rkdvnfma90@gmail.com>
2021-12-03 16:14:01 +0900

    DVT-#9 라우터 초기 설정
    - 라우터 초기 설정
    - react-router-dom 라이브러리 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as ReactDOM from "react-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const container = document.getElementById("app");
-ReactDOM.render(<App />, container);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  container
+);
