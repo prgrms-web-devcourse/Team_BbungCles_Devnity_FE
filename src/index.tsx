010510dc7fba90dcd765635e53b740982398aca1
datalater <the7mincheol@gmail.com>
2021-12-04 13:53:59 +0900

    DVT-#23 reset.css 및 글로벌 스타일 추가
    - 크로스 브라우징이 가능하도록 reset.css로 유저 에이전트 스타일 시트를
    초기화한다.
    - 표준 HTML 엘리먼트에 공통적으로 스타일을 적용하기 위해 글로벌 스타일을
    추가한다.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
+import { Global } from "@emotion/react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import globalStyles from "./assets/globalStyles";
 
 const container = document.getElementById("app");
 ReactDOM.render(
   <BrowserRouter>
+    <Global styles={globalStyles} />
     <App />
   </BrowserRouter>,
   container
