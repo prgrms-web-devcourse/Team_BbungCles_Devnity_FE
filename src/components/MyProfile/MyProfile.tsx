fabc0ce41f3468f4b2a8dd22baa9de04284fec8f
aB <rkdvnfma90@gmail.com>
2021-12-08 15:50:37 +0900

    DVT-#41 마이페이지 프로필 레이아웃 추가
    - 마이페이지 프로필 레이아웃 구성
    - react-markdown 추가
    - lint 룰 추가 (jsx-a11y/label-has-associated-control)
      - label과 input을 분리하여 사용하기 위함
    - lint 룰 추가 (react-hooks )
      - 훅의 의존성 배열을 체크하기 위함

--- src/components/MyProfile/MyProfile.tsx
+++ src/components/MyProfile/MyProfile.tsx
@@ -1,73 +1,181 @@
-import { Container, HiddenParagraph, MyProfileForm } from "./styles";
+import { ChangeEvent, useCallback, useRef, useState } from "react";
+import {
+  Container,
+  HiddenInput,
+  MyProfileForm,
+  ProfileImage,
+  Input,
+  Select,
+  RowContainer,
+  ColumnContainer,
+  Textarea,
+  Button,
+} from "./styles";
+import { IProps } from "./types";
 
-const MyProfile = () => {
-  const mbtis: string[] = ["ISTJ", "ISTP"];
+interface SelectOption {
+  value: string | number;
+  label: string;
+}
+
+// TODO : 추후 data로 분리해야 함 (백엔드 API로 받아올 가능성도 있음)
+const courses: SelectOption[] = [
+  { value: "FE", label: "프론트엔드" },
+  { value: "BE", label: "백엔드" },
+  { value: "AI", label: "인공지능" },
+];
+const generations: SelectOption[] = [
+  { value: "1", label: "1기" },
+  { value: "2", label: "2기" },
+];
+
+const mbtis: string[] = [
+  "INTJ",
+  "INTP",
+  "ISTJ",
+  "ISFJ",
+  "INFJ",
+  "INFP",
+  "ISTP",
+  "ISFP",
+  "ENTJ",
+  "ENTP",
+  "ESTJ",
+  "ESFJ",
+  "ENFA",
+  "ENFP",
+  "ESTP",
+  "ESFP",
+];
+
+const MyProfile = ({ formik }: IProps) => {
+  const [image, setImage] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>();
+
+  const handleImageClick = useCallback(() => {
+    inputRef.current.click();
+  }, []);
+
+  const handleImageChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const result = reader.result as string;
+        setImage(result);
+        formik.setFieldValue("profileImgUrl", result);
+      };
+      reader.readAsDataURL(file);
+    },
+    [formik]
+  );
 
   return (
     <Container>
-      <MyProfileForm>
-        <img
-          src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=100"
-          alt="profile"
+      <ProfileImage
+        src={image || "https://source.unsplash.com/random"}
+        alt="profile"
+        onClick={handleImageClick}
+      />
+      <MyProfileForm onSubmit={formik.handleSubmit}>
+        <HiddenInput
+          ref={inputRef}
+          type="file"
+          name="profileImgUrl"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+        <RowContainer>
+          <ColumnContainer>
+            <label htmlFor="name">이름</label>
+            <Input
+              type="text"
+              name="name"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
+          </ColumnContainer>
+
+          <ColumnContainer>
+            <label htmlFor="email">이메일</label>
+            <Input
+              type="text"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+          </ColumnContainer>
+        </RowContainer>
+
+        <RowContainer>
+          <ColumnContainer>
+            <label htmlFor="generation">기수</label>
+            <Select name="generation">
+              {generations.map(({ value, label }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </Select>
+          </ColumnContainer>
+
+          <ColumnContainer>
+            <label htmlFor="course">코스</label>
+            <Select name="course">
+              {courses.map(({ value, label }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </Select>
+          </ColumnContainer>
+        </RowContainer>
+
+        <label htmlFor="mbti">MBTI</label>
+        <Select name="mbti">
+          <option value="">MBTI를 선택해주세요</option>
+          {mbtis.map((mbti) => (
+            <option key={mbti} value={mbti}>
+              {mbti}
+            </option>
+          ))}
+        </Select>
+
+        <label htmlFor="githubUrl">깃허브</label>
+        <Input
+          type="text"
+          name="githubUrl"
+          onChange={formik.handleChange}
+          value={formik.values.githubUrl}
         />
 
-        <label htmlFor="profileImgUrl">
-          <HiddenParagraph>프로필사진</HiddenParagraph>
-          <input type="file" name="profileImgUrl" accept="image/*" />
-        </label>
+        <label htmlFor="blogUrl">블로그</label>
+        <Input
+          type="text"
+          name="blogUrl"
+          onChange={formik.handleChange}
+          value={formik.values.blogUrl}
+        />
 
-        <label htmlFor="name">
-          이름
-          <input type="text" name="name" />
-        </label>
-
-        <label htmlFor="email">
-          이메일
-          <input type="text" name="email" />
-        </label>
-
-        <label htmlFor="generation">
-          기수
-          <input type="text" name="generation" />
-        </label>
-
-        <label htmlFor="course">
-          코스
-          <input type="text" name="course" />
-        </label>
-
-        <label htmlFor="mbti">
-          MBTI
-          <select name="mbti">
-            <option value="">MBTI를 선택해주세요</option>
-            {mbtis.map((mbti) => (
-              <option key={mbti} value={mbti}>
-                {mbti}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label htmlFor="githubUrl">
-          깃허브
-          <input type="text" name="githubUrl" />
-        </label>
-
-        <label htmlFor="blogUrl">
-          블로그
-          <input type="text" name="blogUrl" />
-        </label>
-
-        <label htmlFor="summary">
-          한줄 소개
-          <input type="text" name="summary" />
-        </label>
-
-        <div>자기소개 MD 영역</div>
+        <label htmlFor="summary">한줄 소개</label>
+        <Input
+          type="text"
+          name="summary"
+          onChange={formik.handleChange}
+          value={formik.values.summary}
+        />
+        {/* TODO: reset.css 처리 및 포커스에 따라 textarea, ReactMarkdown을 구분하여 보여줘도 될듯, 이 부분은 팀원들과 같이 협의 후 진행 */}
+        <label htmlFor="description">자기소개</label>
+        <Textarea
+          name="description"
+          onChange={formik.handleChange}
+          value={formik.values.description}
+        />
 
         <div>내 위치 지도 API 영역</div>
 
-        <button type="submit">수정</button>
+        <Button type="submit">수정</Button>
       </MyProfileForm>
     </Container>
   );
--- src/components/MyProfile/MyProfileContainer.tsx
+++ src/components/MyProfile/MyProfileContainer.tsx
@@ -1,7 +1,32 @@
+import { useFormik, FormikProps } from "formik";
 import MyProfile from "./MyProfile";
+import { FormValues } from "./types";
 
 const MyProfileContainer = () => {
-  return <MyProfile />;
+  const formik: FormikProps<FormValues> = useFormik<FormValues>({
+    initialValues: {
+      name: "",
+      email: "",
+      course: "",
+      generation: null,
+      role: "",
+      profileImgUrl: "",
+      mbti: "",
+      blogUrl: "",
+      githubUrl: "",
+      summary: "",
+      latitude: null,
+      longitude: null,
+      description: "",
+    },
+    onSubmit: (values) => {
+      // TODO: 추후 API 연동되면 console.log 지울 것
+      // eslint-disable-next-line no-console
+      console.log(values);
+    },
+  });
+
+  return <MyProfile formik={formik} />;
 };
 
 export default MyProfileContainer;
