186cd5567cdad0d6fb6d3f39886d46ff61e05071
aB <rkdvnfma90@gmail.com>
2021-12-16 23:29:39 +0900

    DVT-#105 필요없는 주석 제거

--- src/components/UserDetail/Comment/index.tsx
+++ src/components/UserDetail/Comment/index.tsx
@@ -176,31 +176,6 @@ const Comment = ({ comment, isChild, introductionId }: IProps) => {
             </>
           )}
         </ButtonContainer>
-
-        {/* {myProfile.user.userId === comment.writer.userId &&
-          comment.status !== common.commentStatus.DELETED && (
-            <ButtonContainer>
-              {!isChild && (
-                <IconButton role="button" onClick={handleChildCommentClick}>
-                  <FaComments size={24} />
-                </IconButton>
-              )}
-
-              <IconButton role="button" onClick={handleModifyClick}>
-                <BsFillKeyboardFill size={24} />
-              </IconButton>
-
-              <IconButton
-                role="button"
-                onClick={handleDeleteClick(comment.commentId)}
-              >
-                <MdDeleteSweep size={24} />
-              </IconButton>
-            </ButtonContainer>
-
-
-
-          )} */}
       </CommentContainer>
 
       {isChildCommentClick && (
