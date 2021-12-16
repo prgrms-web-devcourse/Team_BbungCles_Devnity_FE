ac84e8a19f22e4ae85bd63fd6b46027cc5b9e7d4
aB <rkdvnfma90@gmail.com>
2021-12-17 00:00:01 +0900

    DVT-#105 lint 수정

--- src/components/Login/LoginContainer.tsx
+++ src/components/Login/LoginContainer.tsx
@@ -1,6 +1,7 @@
 import { useFormik, FormikProps } from "formik";
 import { useHistory } from "react-router-dom";
 import { useMutation } from "react-query";
+import { useSetRecoilState } from "recoil";
 import { FormValues } from "./types";
 import { requestLogin } from "../../utils/apis";
 import { loginValidator } from "../../utils/yups/login";
@@ -8,7 +9,6 @@ import { MutationData, MutationError } from "../../types/commonTypes";
 import { login, errorCode } from "../../constants";
 import Login from "./Login";
 import { useLocalStorage } from "../../hooks";
-import { useSetRecoilState } from "recoil";
 import { authState } from "../../atoms/auth";
 
 const LoginContainer = () => {
