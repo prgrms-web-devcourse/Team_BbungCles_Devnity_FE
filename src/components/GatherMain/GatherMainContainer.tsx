f3b93e1ccd9caa369a0dbe810ba3d60a7a9e394b
jungmin <qkrwjdals302@naver.com>
2021-12-17 17:37:03 +0900

    DVT-#97 모집게시판 메인 페이지 API 연동
    - 더미 데이터 제거
    - 페이징 조회 API 연동했으나 기본값으로 설정돼있어서 수정 필요

--- src/components/GatherMain/GatherMainContainer.tsx
+++ src/components/GatherMain/GatherMainContainer.tsx
@@ -1,60 +1,21 @@
 import { useState } from "react";
 import GatherMain from "./GatherMain";
-
-export const dummy = [
-  {
-    status: "GATHERING",
-    gatherId: 1,
-    category: "PROJECT",
-    title: "자스 같이 해용",
-    createdDate: "2021-12-04",
-    deadLine: "2021-12-11",
-    applicantLimit: 8,
-    applicantCount: 3,
-    view: 67,
-    name: "뿡돌이",
-    course: "BE",
-    generation: 2,
-    profileImgUrl: "https://picsum.photos/200/400",
-  },
-  {
-    status: "CLOSED",
-    gatherId: 2,
-    category: "STUDY",
-    title: "백엔드 스터디 모집",
-    createdDate: "2021-11-04",
-    deadLine: "2021-11-23",
-    applicantLimit: 8,
-    applicantCount: 3,
-    view: 58,
-    name: "뿡수",
-    course: "FE",
-    generation: 1,
-    profileImgUrl: "https://picsum.photos/200/400",
-  },
-  {
-    status: "GATHERING",
-    gatherId: 3,
-    category: "STUDY",
-    title: "백엔드 스터디 모집한다구요~~~",
-    createdDate: "2021-10-04",
-    deadLine: "2021-10-23",
-    applicantLimit: 15,
-    applicantCount: 4,
-    view: 30,
-    name: "뿡민이",
-    course: "BE",
-    generation: 5,
-    profileImgUrl: "https://picsum.photos/200/400",
-  },
-];
+import useFilteredGathers from "../../hooks/useFilteredGathers";
 
 export interface IProps {
   selectedCategory?: string;
 }
 
 const GatherContainer = ({ selectedCategory }: IProps) => {
+  const [filters] = useState({
+    category: selectedCategory || "",
+    lastId: "",
+    size: "10",
+  });
+
   const [modalVisible, setModalVisible] = useState(false);
+
+  const { gatherData } = useFilteredGathers(filters);
 
   const handleVisibleModal = (isModalVisible: boolean) => {
     setModalVisible(isModalVisible);
@@ -62,7 +23,7 @@ const GatherContainer = ({ selectedCategory }: IProps) => {
 
   return (
     <GatherMain
-      gatherData={dummy}
+      gatherData={gatherData}
       selectedCategory={selectedCategory}
       modalVisible={modalVisible}
       handleVisibleModal={handleVisibleModal}
