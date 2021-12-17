c1ac1872d4d3a544a202803dcaa65c1063d3fda7
jungmin <qkrwjdals302@naver.com>
2021-12-17 17:43:40 +0900

    DVT-#97 기존 모집 등록 API 삭제

--- src/utils/apis/makeGather.ts
+++
@@ -1,6 +0,0 @@
-import necessaryAuthAxiosInstance from "./necessaryAuthAxiosInstance";
-
-export const requestPostAddGather = (gatherContent) => {
-  console.log({ gatherContent });
-  return necessaryAuthAxiosInstance.post("v1/gathers", gatherContent);
-};
