13a0a590d42032569aeb1d5ab9524bb163d6ee00
jungmin <qkrwjdals302@naver.com>
2021-12-17 17:33:41 +0900

    DVT-#97 GatherDetail 스타일추가 및 인터페이스 추가
    - 모집 마감, 정원 초과, 삭제된 모집일 경우 정보를 보여주기 위해 스타일 추가
    - 댓글 관련 API 인터페이스 추가

--- src/components/GatherDetail/types.ts
+++ src/components/GatherDetail/types.ts
@@ -1,65 +1,23 @@
-interface SimpleUserInfo {
-  userId: string;
-  name: string;
-  course: string;
-  generation: number;
-  profileImgUrl: string;
-  role: string;
+import { Gather } from "../../types/gather";
+import { SubmitProps } from "../CommentForm/CommentForm";
+
+export interface DeleteCommentProps {
+  gatherId: number;
+  commentId: number;
 }
 
+export interface EditommentProps {
+  gatherId: number;
+  commentId: number;
+  content: string;
+}
 export interface Props {
-  gatherData: {
-    gatherId: number;
-    category: string;
-    title: string;
-    hostName: string;
-    hostProfile: string;
-    hostCourse: string;
-    hostGeneration: number;
-    createdDate: string;
-    deadLine: string;
-    view: number;
-    period: number;
-    applicantLimit: number;
-    applicantCount: number;
-    applicants: Array<SimpleUserInfo>;
-    content: string;
-    comments?: [
-      {
-        commentId: number;
-        content: string;
-        createdAt: string;
-        updatedAt: string;
-        status: string;
-
-        simpleUserInfo: {
-          userId: string;
-          name: string;
-          course: string;
-          generation: number;
-          profileImgUrl: string;
-          role: string;
-        };
-
-        children?: [
-          {
-            commentId: number;
-            parent: number;
-            content: string;
-            createdAt: string;
-            updatedAt: string;
-
-            simpleUserInfo: SimpleUserInfo;
-          }
-        ];
-      }
-    ];
-  };
+  gatherData: Gather;
   handleGatherDelete: (arg: number) => void;
   handleGatherClose: (arg: number) => void;
-  handleGatherCancel: (arg1: number, arg2: string) => void;
+  handleGatherCancel: (arg1: number) => void;
   handleGatherApply: (arg1: number) => void;
-  handleCommentSubmit: (arg: string) => void;
-  handleCommentDelete: (arg: number) => void;
-  handleCommentEdit: (arg: number) => void;
+  handleCommentSubmit: (arg: SubmitProps) => void;
+  handleCommentDelete: (arg: DeleteCommentProps) => void;
+  handleCommentEdit: (arg: EditommentProps) => void;
 }
