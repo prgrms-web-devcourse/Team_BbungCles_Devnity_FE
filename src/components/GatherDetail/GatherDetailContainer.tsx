45bee05b2c6a670405b4513f89f1b9f24cadd41f
jungmin <qkrwjdals302@naver.com>
2021-12-17 17:31:29 +0900

    DVT-#97 GatherDetail 더미데이터 제거 및 API연동
    - 해당 id의 모임 정보 불러오기
    - 모임 삭제, 모임 신청
    - 댓글 등록, 댓글 삭제, 댓글 수정
    - 댓글 수정은 수정 레이아웃이 아직 없어서 주석 처리해놓음

--- src/components/GatherDetail/GatherDetailContainer.tsx
+++ src/components/GatherDetail/GatherDetailContainer.tsx
@@ -1,57 +1,36 @@
 // TODO: API 연동 후 콘솔창은 지울 예정이다.
 /* eslint-disable no-console */
-import useGatherApply from "../../hooks/useGatherApply";
-import GatherDetail from "./GatherDetail";
 
-const dummy = {
-  gatherId: 1,
-  category: "스터디",
-  title: "자스 같이 공부해요",
-  hostName: "박뿡치",
-  hostCourse: "FE",
-  hostProfile: "https://picsum.photos/200/400",
-  hostGeneration: 1,
-  createdDate: "2021-12-04",
-  deadLine: "2021-12-11",
-  view: 56,
-  period: 20211212,
-  applicantLimit: 8,
-  applicantCount: 3,
-  applicants: [
-    {
-      userId: "123",
-      name: "뿡순이",
-      course: "FE",
-      generation: 1,
-      profileImgUrl: "https://picsum.photos/200/400",
-      role: "STUDENT",
-    },
-    {
-      userId: "123",
-      name: "뿡돌이",
-      course: "BE",
-      generation: 2,
-      profileImgUrl: "https://picsum.photos/200/400",
-      role: "STUDENT",
-    },
-    {
-      userId: "123",
-      name: "뿡뿡이",
-      course: "FE",
-      generation: 1,
-      profileImgUrl: "https://picsum.photos/200/400",
-      role: "STUDENT",
-    },
-  ],
-  place: "강남역",
-  content:
-    "안녕하세요?자바스크립트 스터디를 모집하고자 합니다.주1회, 목요일 또는 수요일 저녁 8시 또는 9시에 1시간 정도 진행을 할 예정(원격으로 발표형식)입니다.많은 지원 부탁드려요!",
-};
+import { useParams } from "react-router-dom";
+import useApplyGather from "../../hooks/useApplyGather";
+import useCancelAppliedGather from "../../hooks/useCancelApplyGather";
+import useGetGatherDetail from "../../hooks/useGetGatherDetail";
+
+import GatherDetail from "./GatherDetail";
+import useCreateGatherComment from "../../hooks/useCreateGatherComment";
+import useDeleteGather from "../../hooks/useDeleteGather";
+import useDeleteComment from "../../hooks/useDeleteComment";
+import useEditComment from "../../hooks/useEditComment";
 
 const GatherDetailContainer = () => {
-  // TODO: 추후 API 호출 로직 작성 필요
+  const { id } = useParams<{ id: string }>();
+
+  const { data } = useGetGatherDetail(id);
+
+  console.log(data);
+
+  const { cancelAppliedGather } = useCancelAppliedGather();
+  const { applyGather } = useApplyGather();
+  const { createComment } = useCreateGatherComment();
+  const { deleteGather } = useDeleteGather();
+  const { deleteComment } = useDeleteComment();
+  const { editComment } = useEditComment();
+
+  // TODO: API 호출 로직 작성 필요
+
   const handleGatherDelete = (gatherId) => {
     console.log("모임 삭제", gatherId);
+    deleteGather(gatherId);
   };
 
   const handleGatherClose = (gatherId) => {
@@ -59,37 +38,46 @@ const GatherDetailContainer = () => {
   };
 
   const handleGatherApply = (gatherId) => {
-    useGatherApply(gatherId);
+    applyGather(gatherId);
     console.log("모임 신청", gatherId);
   };
 
-  const handleGatherCancel = (gatherId, userId) => {
-    console.log("모임 신청", gatherId, userId);
+  const handleGatherCancel = (gatherId) => {
+    cancelAppliedGather(gatherId);
+
+    console.log("모임 취소", gatherId);
   };
 
-  const handleCommentSubmit = (gatherId) => {
-    console.log("댓글 등록", gatherId);
+  const handleCommentSubmit = (submitValue) => {
+    console.log("댓글 등록", submitValue);
+    createComment(submitValue);
   };
 
-  const handleCommentDelete = (commentId) => {
-    console.log("댓글 삭제", commentId);
+  const handleCommentDelete = (deleteValue) => {
+    console.log("댓글 삭제", deleteValue);
+    deleteComment(deleteValue);
   };
 
-  const handleCommentEdit = (commentId) => {
-    console.log("댓글 수정", commentId);
+  const handleCommentEdit = (editValue) => {
+    console.log("댓글 수정", editValue);
+    editComment(editValue);
   };
 
   return (
-    <GatherDetail
-      gatherData={dummy}
-      handleCommentDelete={handleCommentDelete}
-      handleCommentEdit={handleCommentEdit}
-      handleGatherApply={handleGatherApply}
-      handleCommentSubmit={handleCommentSubmit}
-      handleGatherCancel={handleGatherCancel}
-      handleGatherClose={handleGatherClose}
-      handleGatherDelete={handleGatherDelete}
-    />
+    <div>
+      {data ? (
+        <GatherDetail
+          gatherData={data?.data}
+          handleCommentDelete={handleCommentDelete}
+          handleCommentEdit={handleCommentEdit}
+          handleGatherApply={handleGatherApply}
+          handleCommentSubmit={handleCommentSubmit}
+          handleGatherCancel={handleGatherCancel}
+          handleGatherClose={handleGatherClose}
+          handleGatherDelete={handleGatherDelete}
+        />
+      ) : undefined}
+    </div>
   );
 };
 
