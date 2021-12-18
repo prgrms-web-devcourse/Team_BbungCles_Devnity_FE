2ba770c74ecaa9d7beb7c883615682e4862e237b
datalater <the7mincheol@gmail.com>
2021-12-19 03:23:44 +0900

    DVT-#115 맵각코 초기 위치를 프로필에서 등록한 위치로 렌더링

--- src/components/MapgakcoMap/MapgakcoMapContainer.tsx
+++ src/components/MapgakcoMap/MapgakcoMapContainer.tsx
@@ -1,11 +1,11 @@
 import { useRecoilValue } from "recoil";
 import randomUserMapInfo from "../../../fixtures/userMapInfo";
-import { currentUserState } from "../../atoms/user";
+import { globalMyProfile } from "../../atoms/user";
 import { common } from "../../constants";
 import MapgakcoMap from "./MapgakcoMap";
 
 const MapgakcoMapContainer = () => {
-  const currentUser = useRecoilValue(currentUserState);
+  const currentUser = useRecoilValue(globalMyProfile);
 
   const userMapInfos = Array.from({ length: 120 }, () => randomUserMapInfo());
 
