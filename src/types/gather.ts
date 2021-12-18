05036ff1d561224dfa12cccdd91fa0df5f9c7261
jungmin <qkrwjdals302@naver.com>
2021-12-19 05:01:49 +0900

    DVT-#127 맵각코도 같이 보여주기 위해 type의 gather 수정

--- src/types/gather.ts
+++ src/types/gather.ts
@@ -2,20 +2,45 @@ import { Comment } from "./comment";
 import { SimpleUserInfo } from "./simpleUserInfo";
 
 export interface Gather {
-  gatherId: number;
+  gatherId?: number;
   status: string;
   title: string;
   content?: string;
-  category: string;
-  deadline: string;
+  category?: string;
+  deadline?: string;
   createdAt: string;
   modifiedAt?: string;
   author: SimpleUserInfo;
   applicantLimit: number;
-  view: number;
+  view?: number;
   applicantCount: number;
-  commentCount: number;
-  isApplied: boolean;
+  commentCount?: number;
+  isApplied?: boolean;
   participants?: Array<SimpleUserInfo>;
+  comments?: Array<Comment>;
+
+  mapgakcoId?: number;
+  location?: string;
+  latitude?: number;
+  longitude?: number;
+  meetingAt?: string;
+}
+
+export interface Mapgakco {
+  mapgakcoId: number;
+  applicantLimit: number;
+  applicantCount: number;
+  status: string;
+  title: string;
+  content: string;
+  location: string;
+  latitude: number;
+  longitude: number;
+  meetingAt: number;
+  createdAt: number;
+  modifiedAt?: number;
+
+  author: SimpleUserInfo;
+  applicants?: Array<SimpleUserInfo>;
   comments?: Array<Comment>;
 }
