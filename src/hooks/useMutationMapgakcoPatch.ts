5ec5e72f4006a43d779fc695881e302787912ee8
datalater <the7mincheol@gmail.com>
2021-12-21 01:27:47 +0900

    DVT-#165 맵각코 등록하면 맵각코 마커 자동 리렌더링

--- src/hooks/useMutationMapgakcoPatch.ts
+++ src/hooks/useMutationMapgakcoPatch.ts
@@ -11,6 +11,7 @@ const useMutationMapgakcoPatch = (id: string) => {
     {
       onSuccess: () => {
         queryClient.invalidateQueries("mapgakcoDetail");
+        queryClient.invalidateQueries("mapgakcos");
       },
     }
   );
