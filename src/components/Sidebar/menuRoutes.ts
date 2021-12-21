8163f41ac9cb0f9cdeebf0c78cd67b5b87907c70
aB <rkdvnfma90@gmail.com>
2021-12-21 11:00:57 +0900

    DVT-#172 사이드바 스터디, 동아리, 프로젝트 1 depth 안으로 넣어서 구분

--- src/components/Sidebar/menuRoutes.ts
+++ src/components/Sidebar/menuRoutes.ts
@@ -5,40 +5,48 @@ export default [
     name: "데둥이 소개",
     path: routes.USERLIST,
     shouldAuth: false,
+    isChild: false,
   },
   {
     name: "데둥여지도",
     path: routes.USERSMAP,
     shouldAuth: false,
+    isChild: false,
   },
   {
     name: "모집 게시판",
     path: routes.GATHERLIST,
     shouldAuth: false,
+    isChild: false,
   },
   {
     name: "스터디",
     path: routes.GATHERLIST_STUDY,
     shouldAuth: false,
+    isChild: true,
   },
   {
     name: "동아리",
     path: routes.GATHERLIST_CLUB,
     shouldAuth: false,
+    isChild: true,
   },
   {
     name: "프로젝트",
     path: routes.GATHERLIST_PROJECT,
     shouldAuth: false,
+    isChild: true,
   },
   {
     name: "맵각코",
     path: routes.MAPGAKCOLIST,
     shouldAuth: false,
+    isChild: false,
   },
   {
     name: "관리자",
     path: routes.ADMIN,
     shouldAuth: true,
+    isChild: false,
   },
 ];
