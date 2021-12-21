b837541fb7bfecdb651d4795e0ec321202d47b7a
aB <rkdvnfma90@gmail.com>
2021-12-21 11:21:14 +0900

    DVT-#172 메인화면에서 자기소개 클릭시 상세화면으로 이동

--- src/components/UserCardList/UserCardList.tsx
+++ src/components/UserCardList/UserCardList.tsx
@@ -1,19 +1,36 @@
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
 import { UserInfo } from "../../types/userInfo";
 import UserCard from "../UserCard/UserCard";
 import Text from "../base/Text";
+import { routes } from "../../constants";
 
 interface Props {
   userInfos: UserInfo[];
 }
 
 const UserCardList = ({ userInfos }: Props) => {
+  const history = useHistory();
+
+  const handleUserCardClick = useCallback(
+    (userId) => () => {
+      history.push(`${routes.USERLIST}/${userId}`);
+    },
+    [history]
+  );
+
   return (
     <div>
       {(userInfos || []).length ? (
         <ul>
           {userInfos?.map((userInfo) => (
             <li key={userInfo.user.userId}>
-              <UserCard userInfo={userInfo} />
+              <UserCard
+                userInfo={userInfo}
+                onClick={handleUserCardClick(
+                  userInfo.introduction.introductionId
+                )}
+              />
             </li>
           ))}
         </ul>
